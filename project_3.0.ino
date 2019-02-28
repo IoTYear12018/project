4d93eb87427fdb7a939317b5a69e6f204296c8cb
Tara McDonagh <taramcdonagh@hotmail.com>
2019-02-28 14:14:44 +0000

    Project code

---
+++ project_3.0.ino
@@ -0,0 +1,45 @@
+
+#include <math.h>
+
+const int B = 4275;               // B value of the thermistor
+const int R0 = 100000;            // R0 = 100k
+const int pinTempSensor = A0;
+
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
+
+void setup() 
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    
+    lcd.setRGB(colorR, colorG, colorB);
+    
+    // Print a message to the LCD.
+    lcd.print("temperature = ");
+
+    delay(1000);
+}
+
+void loop() {
+    lcd.setCursor(0, 1);
+    int a = analogRead(pinTempSensor);
+
+    float R = 1023.0/a-1.0;
+    R = R0*R;
+
+    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;
+    lcd.println(temperature);
+
+    delay(100);
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
